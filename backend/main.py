ae77eef2e5d1a5d251696e78234bd0b5fae5d9a0
Asan <fake@mail.com>
2024-08-17 00:18:57 +0600

    Add frontend
    - Adds demo Vue.js project

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,20 @@
 from typing import Annotated
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime, timedelta
 from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import BaseModel, Field, ConfigDict
 from pydantic.functional_validators import BeforeValidator
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:5173'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 client = AsyncIOMotorClient("mongodb://localhost:27017")
 db = client.myDatabase
---
+++ frontend/src/components/Table.vue
@@ -0,0 +1,26 @@
+<script>
+
+import apiClient from '@/services/axios';
+
+export default {
+  data() {
+    return {
+        resp: null
+    }
+  },
+  methods: {
+    async sendSchedule() {
+        const response = await apiClient.post('/schedules', {
+            person: 'Bryan',
+            subject: 'geography',
+        })
+        this.resp = response.data
+    }
+  }
+}
+</script>
+
+<template>
+    <button @click="sendSchedule()">click me</button>
+    <p>{{ resp }}</p>
+</template>
