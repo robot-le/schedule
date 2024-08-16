fa5eb1bd59918571d3b2b4013b04fd6711f47513
Asan <fake@mail.com>
2024-08-16 23:22:49 +0600

    Initial commit
    - Adds FastAPI simple single-file demo app

---
+++ backend/main.py
@@ -0,0 +1,34 @@
+from typing import Annotated
+from fastapi import FastAPI
+from datetime import datetime, timedelta
+from motor.motor_asyncio import AsyncIOMotorClient
+from pydantic import BaseModel, Field, ConfigDict
+from pydantic.functional_validators import BeforeValidator
+
+app = FastAPI()
+
+client = AsyncIOMotorClient("mongodb://localhost:27017")
+db = client.myDatabase
+collection = db.myCollection
+
+PyObjectId = Annotated[str, BeforeValidator(str)]
+
+class Schedule(BaseModel):
+    id: PyObjectId | None = Field(alias="_id", default=None)
+    person: str
+    subject: str
+    date_start: datetime = Field(default=datetime.today())
+    date_finish: datetime = Field(default=datetime.today() + timedelta(days=10))
+    model_config = ConfigDict(populate_by_name=True)
+
+class ScheduleCollection(BaseModel):
+    schedules: list[Schedule]
+
+@app.post('/schedules')
+async def create_schedule(item: Schedule):
+    result = await collection.insert_one(item.model_dump())
+    return {"id": str(result.inserted_id)}
+
+@app.get("/schedules")
+async def read_schedules():
+    return ScheduleCollection(schedules=await collection.find().to_list(100))
